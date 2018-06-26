e2247385c797983bd8ba45cc15ded4ab71bed05b
rednivrug <gurvinder630@yahoo.com>
2018-06-26 22:56:53 +0530

    Add files via upload

---
+++ Game_util_selenium.py
@@ -0,0 +1,152 @@
+import os
+from PIL import Image
+import pytesseract
+import selenium
+from selenium import webdriver
+import cv2
+import time
+from pywinauto import application
+import csv
+from grabscreen import grab_screen
+import io
+import numpy as np
+
+os.chdir('C:\\Users\\royal\\Downloads\\Compressed\\DeepGamingAI_FIFARL-master')
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+
+chromedriver = "C:\\Users\\royal\\Downloads\\Programs\\chromedriver_win32\\chromedriver.exe"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+
+rate=[2.25, 2.50, 2.75, 3.00, 3.25, 3.50, 3.75, 4.00, 4.25, 4.50, 4.75, 5.00, 5.25, 5.50, 5.75, 6.00, 6.25, 6.50, 6.75, 7.00, 7.25, 7.50, 7.75, 8.00, 8.25, 8.50, 8.75, 9.00, 9.25, 9.50, 9.75]
+
+def open_page_start(driver):
+    #open the chrome browser with site and then maximize the window and click on play now.
+    driver.get("https://sffed-education.org/chairthefed/WebGamePlay.html")
+    driver.maximize_window()
+    driver.find_elements_by_xpath('//*[@id="play_now_button"]')[0].click()
+    #time.sleep(5)
+    #driver.execute_script("window.scrollTo(0, 100)")
+
+def get_to_foreground():
+    ##get the window to foreground.
+    app = application.Application()
+    app.connect(title_re=".*%s.*" % "Chair the Fed")
+    app_dialog = app.top_window_()
+    app_dialog.Minimize()
+    app_dialog.Restore()
+
+def click_go(driver):
+    driver.find_elements_by_xpath('//*[@id="go_button_anchor"]')[0].click()
+    time.sleep(8)
+
+def click_raise(driver):
+    driver.find_elements_by_xpath('//*[@id="increase_button_anchor"]')[0].click()
+    time.sleep(1)
+
+def click_cut(driver):
+    driver.find_elements_by_xpath('//*[@id="decrease_button_anchor"]')[0].click()
+    time.sleep(1)
+
+def get_news(screen):
+    news = screen[2:100,451:1086]
+    i = Image.fromarray(news.astype('uint8'), 'RGB')
+    st = pytesseract.image_to_string(i, lang = 'eng')
+    return st
+
+def get_quaters(screen):
+    quat = screen[158:202,1190:1249]
+    i = Image.fromarray(quat.astype('uint8'), 'RGB')
+    st = pytesseract.image_to_string(i, lang = 'eng')
+    return st
+
+def get_fed_rates(screen):
+    fed = screen[263:341,1108:1277]
+    i = Image.fromarray(fed.astype('uint8'), 'RGB')
+    st = pytesseract.image_to_string(i, lang = 'eng')
+    return st
+
+def get_unemploy_rate(screen):
+    unemp = screen[411:488,1108:1277]
+    i = Image.fromarray(unemp.astype('uint8'), 'RGB')
+    st = pytesseract.image_to_string(i, lang = 'eng')
+    return st
+
+def get_inflate_rate(screen):
+    inflat = screen[559:610,1108:1277]
+    i = Image.fromarray(inflat.astype('uint8'), 'RGB')
+    st = pytesseract.image_to_string(i, lang = 'eng')
+    return st
+
+def get_last_msg(driver):
+    screen = screen_grab(driver)
+    cv2.imwrite('record\\last'+'.PNG',screen)
+    last = screen[45:137,327:1045]
+    i = Image.fromarray(last.astype('uint8'), 'RGB')
+    st = pytesseract.image_to_string(i, lang = 'eng')
+    return st
+
+def set_fed_rate(driver,curr,nex):
+    mov=0
+    if (curr>nex):
+        mov=int((curr-nex)//0.25)
+        for i in range(mov):
+            click_cut(driver)
+    else:
+        mov=int((nex-curr)//0.25)
+        for i in range(mov):
+            click_raise(driver)
+
+def record(i,screen):
+    cv2.imwrite('record\\hh'+str(i)+'.PNG',screen)
+    news=' '.join(get_news(screen).split('\n'))
+    fed=''.join(get_fed_rates(screen).split()).split('%')[0]
+    unemp=''.join(get_unemploy_rate(screen).split()).split('%')[0]
+    infl=''.join(get_inflate_rate(screen).split()).split('%')[0]
+    data=str(16-i)+','+fed+','+unemp+','+infl+','+news
+    print(data)
+    with open('record\\csvex.csv','a', encoding="utf-8") as myFile:
+        myFile.write(data+'\n')
+
+def play_again_win(driver):
+    driver.find_elements_by_xpath('//*[@id="win_play_now_button_anchor"]')[0].click()
+
+def play_again_loss(driver):
+    driver.find_elements_by_xpath('//*[@id="lose_play_now_button_anchor"]')[0].click()
+
+def screen_grab(driver):
+    driver.execute_script("window.scrollTo(0, 100)")
+    data = driver.get_screenshot_as_png()
+    img = Image.open(io.BytesIO(data))
+    return cv2.cvtColor(np.array(img), cv2.COLOR_BGRA2BGR)
+
+    
+def play(driver):
+    time.sleep(18)
+    driver.execute_script("window.scrollTo(0, 100)")
+    rate=[4.00, 4.00, 4.00, 4.25, 4.25, 4.75, 4.25, 4.00, 4.25, 4.50, 4.75, 4.75, 4.25, 4.50, 4.75, 4.75]
+    for i,j in enumerate(rate):
+        #screen = grab_screen(region=None)
+        screen = screen_grab(driver)
+        record(i,screen)
+        fed = get_fed_rates(screen)
+        fed=float(''.join(fed.split()).split('%')[0])
+        print(fed)
+        set_fed_rate(driver,fed,j)
+        click_go(driver)
+
+with open('record\\csvex.csv','a', encoding="utf-8") as myFile:
+    myFile.write('Quarters,FedRate,Unemployment,Inflation,News'+'\n')
+
+print('started!!!')
+open_page_start(driver)
+for i in range(3):
+    play(driver)
+    st = get_last_msg(driver)
+    if st=='Congratulations!':
+        play_again_win(driver)
+    else:
+        play_again_loss(driver)
+    
+print('finished!!!')
---
+++ STATE.py
@@ -0,0 +1,52 @@
+import json
+import torch
+import os
+import numpy as np
+
+torch.manual_seed(1)
+
+##used to definethe state.
+
+class STATE(object):
+    word_to_ix={}
+    news_ll=[]
+    ##json file for saving the loading the news and its unique position
+    news_dic_loc='C:\\Users\\royal\\Downloads\\Compressed\\DeepGamingAI_FIFARL-master\\record'+'\\'+'news_dic.json'
+    news_list_loc='C:\\Users\\royal\\Downloads\\Compressed\\DeepGamingAI_FIFARL-master\\record'+'\\'+'news_list.json'
+
+    ##load the word_to_ix which contain news and its unique index and news_ll contain unique news.
+    def __init__(self):
+        
+        with open(self.news_dic_loc,'r') as fp:
+            self.word_to_ix=json.load(fp)
+        with open(self.news_list_loc,'r') as fp:
+            self.news_ll=json.load(fp)
+
+    ##128 dimension features for the state.
+    def get_features_128(self,news,fed,unemp,infla):
+        word_to_ix=self.word_to_ix
+        news_ll=self.news_ll
+        if news not in news_ll:
+            word_to_ix[news]=len(set(news_ll))
+            news_ll.append(news)
+            with open(self.news_dic_loc,'w') as fp:
+                json.dump(word_to_ix,fp)
+            with open(self.news_list_loc,'w') as fp:
+                json.dump(news_ll,fp)
+        embeds = torch.nn.Embedding(len(word_to_ix), 125)
+        look_tor= torch.tensor([word_to_ix[news]],dtype=torch.long)
+        tor_embed = embeds(look_tor)
+        numpy_embed = tor_embed.data.numpy().reshape(-1,125)
+        fina_embed = np.append(numpy_embed,[fed,unemp,infla])
+        return fina_embed.reshape(-1,128)
+
+    ##4 dimension features for the state.
+    def get_features_4(self,news,fed,unemp,infla):
+        if news not in self.news_ll:
+            self.word_to_ix[news]=len(set(self.news_ll))
+            self.news_ll.append(news)
+            with open(self.news_dic_loc,'w') as fp:
+                json.dump(self.word_to_ix,fp)
+            with open(self.news_list_loc,'w') as fp:
+                json.dump(self.news_ll,fp)
+        return np.array([self.word_to_ix[news],fed,unemp,infla],dtype=np.float).reshape(-1,4)
