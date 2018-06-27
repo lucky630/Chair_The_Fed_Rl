06f78d1c8618913537561b8efb97ccdaf9f9aba7
rednivrug <gurvinder630@yahoo.com>
2018-06-27 19:11:46 +0530

    Update Game_util_selenium.py

--- Game_util_selenium.py
+++ Game_util_selenium.py
@@ -1,3 +1,4 @@
+## this screipt can be used to run the Game 'chair the Fed' using predefined or hardcoded actions.
 import os
 from PIL import Image
 import pytesseract
@@ -5,21 +6,17 @@ import selenium
 from selenium import webdriver
 import cv2
 import time
-from pywinauto import application
 import csv
-from grabscreen import grab_screen
 import io
 import numpy as np
 
-os.chdir('C:\\Users\\royal\\Downloads\\Compressed\\DeepGamingAI_FIFARL-master')
+os.chdir('C:\\Users\\royal\\Downloads\\Compressed')
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 
 chromedriver = "C:\\Users\\royal\\Downloads\\Programs\\chromedriver_win32\\chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
-
-rate=[2.25, 2.50, 2.75, 3.00, 3.25, 3.50, 3.75, 4.00, 4.25, 4.50, 4.75, 5.00, 5.25, 5.50, 5.75, 6.00, 6.25, 6.50, 6.75, 7.00, 7.25, 7.50, 7.75, 8.00, 8.25, 8.50, 8.75, 9.00, 9.25, 9.50, 9.75]
 
 def open_page_start(driver):
     #open the chrome browser with site and then maximize the window and click on play now.
@@ -127,7 +124,6 @@ def play(driver):
     driver.execute_script("window.scrollTo(0, 100)")
     rate=[4.00, 4.00, 4.00, 4.25, 4.25, 4.75, 4.25, 4.00, 4.25, 4.50, 4.75, 4.75, 4.25, 4.50, 4.75, 4.75]
     for i,j in enumerate(rate):
-        #screen = grab_screen(region=None)
         screen = screen_grab(driver)
         record(i,screen)
         fed = get_fed_rates(screen)
